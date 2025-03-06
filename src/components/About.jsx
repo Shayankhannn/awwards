bf90927b2d928b7bc1f912d153d103625735cbc0
shayankhannn <shayannkhannn007@gmail.com>
2025-03-06 15:06:58 +0500

    features

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ useGSAP(()=>{
     const clipAnimation = gsap.timeline({
         scrollTrigger: {
             trigger: "#clip",
-            start: "center center",
+            start: "center top",
             end: "+=800 center",
             scrub: 0.5,
             pin: true,
