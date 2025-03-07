897a230738f5ccb0e4d8f2c2f7f67e93a5326d3d
shayankhannn <shayannkhannn007@gmail.com>
2025-03-07 15:35:03 +0500

    feature

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react"
 import { TiLocationArrow } from "react-icons/ti"
 
 
@@ -24,6 +25,41 @@ const BentoCard = ({ src, title, description }) => {
     )
 }
 
+const BentoTilt = ({children,className= ' '}) => {
+    const [transformStyle,setTransformStyle] = useState('');
+    const itemRef = useRef(null);
+
+    const handleMouseMove = (e) => {
+        if(!itemRef.current) return;
+
+        const {left,top,height,width} = itemRef.current.getBoundingClientRect();
+
+        const relativeX = (e.clientX - left ) / width;
+        const relativeY = (e.clientY - top ) / height;
+
+        const tiltX = (relativeY - 0.5) * 7 ; 
+        const tiltY = (relativeX - 0.5) * -7 ;
+        
+        const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98,0.98,0.98)`
+
+        setTransformStyle(newTransform)
+    };
+    const handleMouseLeave = () => {
+        setTransformStyle('')
+    };
+    return (
+        <div
+         className={className}
+         ref={itemRef} 
+         onMouseMove={handleMouseMove} 
+         onMouseLeave={handleMouseLeave}
+         style={{transform:transformStyle}}
+         >
+            {children}
+        </div>
+    )
+}
+
 const Features = () => {
   return (
     <section className="bg-black pb-52">
@@ -35,48 +71,48 @@ const Features = () => {
     <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, sequi.</p>
     </div>
-    <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+    <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
     <BentoCard
     src="videos/feature-1.mp4"
     title={<>Radia<b>n</b>t</>}
     description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, sequi."
     
     />
-    </div>
+    </BentoTilt>
     <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
-    <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+    <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
         <BentoCard 
         src="videos/feature-2.mp4"
         title={<>Zig<b>m</b>a</>}
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, sequi."
 
         />
-    </div>
-    <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0 ">
+    </BentoTilt>
+    <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0 ">
         <BentoCard 
         src="videos/feature-3.mp4"
         title={<>n<b>e</b>xus</>}
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, sequi."
 
         />
-    </div>
+    </BentoTilt>
 
-    <div className="bento-tilt_1 row-span-1 me-14 md:col-span-1 md:me-0 ">
+    <BentoTilt className="bento-tilt_1 row-span-1 me-14 md:col-span-1 md:me-0 ">
         <BentoCard 
         src="videos/feature-4.mp4"
         title={<>az<b>u</b>re</>}
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, sequi."
 
         />
-    </div>
+    </BentoTilt>
 
-    <div className="bento-tilt_2">
+    <BentoTilt className="bento-tilt_2">
     <div className="flex size-full md:flex-row flex-col justify-between p-5 bg-violet-300">
     <h1 className="bento-title special-font max-w-64">M<b>o</b>re Co<b>m</b>ing S<b>o</b>on</h1>
     <TiLocationArrow className="m-5 scale-[5] self-end"/>
     </div>
-    </div>
-    <div className="bento-tilt_2">
+    </BentoTilt>
+    <BentoTilt className="bento-tilt_2">
         <video 
         src="videos/feature-5.mp4"
         loop
@@ -84,7 +120,7 @@ const Features = () => {
         autoPlay
         className="object-cover object-center size-full"
         />
-    </div>
+    </BentoTilt>
 
     </div>
 </div>
