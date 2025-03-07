de768afc64a1cc5f729ae7b27cbedd93ff59844e
shayankhannn <shayannkhannn007@gmail.com>
2025-03-07 14:40:26 +0500

    #

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ useGSAP(()=>{
         scrollTrigger: {
             trigger: "#clip",
             start: "+=112 center",
-            end: "top center",
+            end: " center",
             scrub: 0.5,
             pin: true,
             pinSpacing: true,
